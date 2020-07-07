14f9a5fd92298634041b358ac3749b9128606bf1
Dima Matusik <k.syndikat@gmail.com>
2020-07-07 16:55:02 +0300

    Updates

---
+++ precache-manifest.ec9d6a0eee836adf1f16a097f659602c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6240a34bffee96b954d5e475f3fae0ea",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "99252ae87528794ed745",
+    "url": "/counter/static/css/main.a5811414.chunk.css"
+  },
+  {
+    "revision": "10953e94f509f3178066",
+    "url": "/counter/static/js/2.3d6c24c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/counter/static/js/2.3d6c24c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99252ae87528794ed745",
+    "url": "/counter/static/js/main.f132a930.chunk.js"
+  },
+  {
+    "revision": "890e0606e2c9e34496c7",
+    "url": "/counter/static/js/runtime-main.01fdcc42.js"
+  }
+]);
